30b1cf6de2a834d4e434baceded687870a1a9dbd
Tomasz Brandys <cyzmenpl@gmail.com>
2024-08-23 15:41:02 +0200

    change user color theme

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
-            $table->string('color_theme')->default('#4944FF');
+            $table->string('color_theme')->default('#F7446A');
             $table->enum('role', ['admin', 'user'])->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
