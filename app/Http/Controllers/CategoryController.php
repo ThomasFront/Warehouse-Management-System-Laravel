d38b34c59d33603a176c39fd244cbd63034af35d
Tomasz Brandys <cyzmenpl@gmail.com>
2024-09-09 19:51:37 +0200

    handle pagination/sort/filters for categories

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\CategoryResource;
 use App\Http\Responses\ApiResponse;
 use App\Http\Services\CategoryService;
 use App\Models\Category;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
@@ -22,9 +23,17 @@ class CategoryController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $categories = $this->categoryService->getCategories();
+        $pageSize = $request->input('pageSize', 25);
+        $sortField = $request->input('sortField', 'created_at');
+        $sortOrder = $request->input('sortOrder', 'asc');
+
+        $filterField = $request->input('filterField');
+        $filterValue = $request->input('filterValue');
+        $filterOperator = $request->input('filterOperator');
+
+        $categories = $this->categoryService->getCategories($pageSize, $sortField, $sortOrder, $filterField, $filterValue, $filterOperator);
 
         return response()->json(new CategoryCollection($categories));
     }
--- app/Http/Services/CategoryService.php
+++ app/Http/Services/CategoryService.php
@@ -8,14 +8,42 @@ use App\Models\Category;
 
 class CategoryService
 {
-    public function getCategories()
+    public function getCategories($pageSize, $sortField, $sortOrder, $filterField, $filterValue, $filterOperator)
     {
-        $perPage = config('pagination.per_page');
+        $query = Category::orderBy('updated_at', 'desc')
+            ->orderBy($sortField, $sortOrder);
 
-        return Category
-            ::orderBy('updated_at', 'desc')
-            ->orderBy('created_at', 'desc')
-            ->paginate($perPage);
+        if ($filterField && $filterOperator && $filterValue) {
+            switch ($filterOperator) {
+                case 'contains':
+                    $query->where($filterField, 'like', "%$filterValue%");
+                    break;
+                case 'equals':
+                    $query->where($filterField, $filterValue);
+                    break;
+                case 'startsWith':
+                    $query->where($filterField, 'like', "$filterValue%");
+                    break;
+                case 'endsWith':
+                    $query->where($filterField, 'like', "%$filterValue");
+                    break;
+                case 'isEmpty':
+                    $query->whereNull($filterField);
+                    break;
+                case 'isNotEmpty':
+                    $query->whereNotNull($filterField);
+                    break;
+                case 'isAnyOf':
+                    if (is_array($filterValue)) {
+                        $query->whereIn($filterField, $filterValue);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        return $query->paginate($pageSize);
     }
 
     public function storeCategory(StoreCategoryRequest $data)
